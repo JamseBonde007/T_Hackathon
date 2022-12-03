107ddb60c744728475ddbe0c1d04eb69368aa1cd
Igor Lengel <igor.lengel@asseco-ce.com>
2022-12-04 00:42:45 +0100

    findExpert API

--- backend/src/main/java/com/thackathon/mim/thk/controller/PersonController.java
+++ backend/src/main/java/com/thackathon/mim/thk/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.thackathon.mim.thk.controller;
 import com.thackathon.mim.thk.entity.Person;
 import com.thackathon.mim.thk.service.PersonService;
 import lombok.NonNull;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -30,5 +33,10 @@ public class PersonController {
     @GetMapping("/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable Long id){
         return ResponseEntity.ok(personService.getPerson(id));
+    }
+
+    @GetMapping("/findExperts")
+    public ResponseEntity<List<Person>> findExperts(Pageable pageable, @RequestParam List<String> skills){
+        return ResponseEntity.ok(personService.findExperts(pageable, skills));
     }
 }
