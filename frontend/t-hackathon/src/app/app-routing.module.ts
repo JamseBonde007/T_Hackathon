cf911c8a5838ead09cebe4f1b31f31c48181afcb
MichalPopovic <popovic.michal626@gmail.com>
2022-12-03 21:36:21 +0100

    postDetail

--- frontend/t-hackathon/src/app/app-routing.module.ts
+++ frontend/t-hackathon/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AllPostsComponent } from './main-page/all-posts/all-posts.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PostDetailComponent } from './main-page/post-detail/post-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent}, 
-  {path: 'mainPage', component: MainPageComponent}
+  {path: 'mainPage', component: MainPageComponent, 
+    children: [
+      {
+      path: 'allPosts', component: AllPostsComponent
+    },
+    {
+      path: 'postDetail', component: PostDetailComponent
+    }
+  ]   
+}
   
 ];
 
--- frontend/t-hackathon/src/app/app.module.ts
+++ frontend/t-hackathon/src/app/app.module.ts
@@ -8,9 +8,12 @@ import { CoreModule } from './core/core.module';
 import { LoginPostComponent } from './shared/post-preview/post-preview.component';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PostDetailComponent } from './main-page/post-detail/post-detail.component';
+import { HeaderComponent } from './main-page/header/header.component';
+import { AllPostsComponent } from './main-page/all-posts/all-posts.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPostComponent, LoginComponent, MainPageComponent],
+  declarations: [AppComponent, LoginPostComponent, LoginComponent, MainPageComponent, PostDetailComponent, HeaderComponent, AllPostsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
